79a099a9f9711109cc1991ad5023a88b356e7d95
Vikas <vikas.softechure@gmail.com>
2024-07-06 14:12:59 +0530

    updates

--- application/views/components/other/add-more/property-documents.php
+++ application/views/components/other/add-more/property-documents.php
@@ -5,15 +5,17 @@ if(count($property_documents)):
 ?>
 <!-- Static Property Documents Card -->
 <div class="card p-4 property-documents clone-template" data-clone-template-id="<?= $property_document_key; ?>">
-        <?php if($property_document_key != 1 ):?>
-        <span class="remove-clone-template-row fa fa-trash" onclick="removeCloneTemplateRow(this)" data-id="<?= $property_document->id ?? '' ?>"></span>
-        <?php endif;?>
+        <span class="remove-clone-template-row fa fa-trash" onclick="removeCloneTemplateRow(this)" data-id="<?= $property_document->id ?? '' ?>" data-type="<?= $property_document_key == 1 ? 'main' : '' ?>"></span>
         <div class="row item-row w-100">
+            <!-- Id -->
+             <input type="hidden" name="property_documents[<?= $property_document_key; ?>][id]" value="<?= $property_document->id ?? '' ?>">
+            <!-- End Id -->
+
             <!-- Title -->
             <div class="col">
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" class="form-control document_title" placeholder="Enter document title" value="<?= $property_document->title ?? '' ?>" name="property_documents[<?= $property_document_key; ?>][title]" value="" required>
+                    <input type="text" class="form-control document_title" placeholder="Enter document title" value="<?= $property_document->title ?? '' ?>" name="property_documents[<?= $property_document_key; ?>][title]" value="">
                 </div>
             </div>
             <!-- End Title -->
@@ -41,7 +43,7 @@ else:
             <div class="col">
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" class="form-control document_title" placeholder="Enter document title" name="property_documents[1][title]" value="" required>
+                    <input type="text" class="form-control document_title" placeholder="Enter document title" name="property_documents[1][title]" value="">
                 </div>
             </div>
             <!-- End Title -->
@@ -50,7 +52,7 @@ else:
             <div class="col">
                 <div class="form-group">
                     <label for="document_file">Document File</label>
-                    <input type="file" class="form-control document_file" name="property_documents[1][document_file]" required>
+                    <input type="file" class="form-control document_file" name="property_documents[1][document_file]">
                 </div>
             </div>
             <!-- End Document File -->
